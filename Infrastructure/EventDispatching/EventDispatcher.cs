fb641d70380d3dc6652b3949fac3dc3bb68e70f0
David Khurtsidze <david.khurtsidze@wandio.com>
2020-08-20 13:37:40 +0400

    fixed namespace in eventdispatcher

--- Infrastructure/EventDispatching/EventDispatcher.cs
+++ Infrastructure/EventDispatching/EventDispatcher.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MyMoon.Domain.Common;
+using System;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MyMoon.Infrastructure.EventDispatching
